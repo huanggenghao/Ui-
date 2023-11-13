43f2a22f8692e4b445ade34fb833dbc673c414b1
huanggenghao <877649270@qq.com>
2023-11-13 13:34:30 +0800

    1.1版本的UI自动化测试，主要内容如下 1)启用setup 2）启用teardown 3）使用类来管理代码，拒绝简单自述

--- case/data.py
+++ case/data.py
@@ -8,37 +8,60 @@ from appium.webdriver.common.appiumby import AppiumBy
 
 
 # 定义Android运行环境
-desired_caps = {
-    "deviceName": "127.0.0.1:16384",  # 启动的设备
-    "automationName": "Appium",  # 使用的自动化引擎，如appium（默认）或Selendroid
-    "platformName": "Android",  # 使用的移动平台，如Android或IOs
-     # adb shell getprop ro.build.version.release，获取设备Android系统版本（即platformVersion）
-    "platformVersion": "10",  # 指定的平台的系统版本，这里为安卓平台，12
-     # dumpsys window | grep mCurrentFocus
-    "appPackage": "com.orcbit.oladanceearphone",  # 被测试App的Package名
-    "appActivity": ".ui.activity.MainActivity",  # 被测试App的Activity名
-    "unicodeKeyboard": True,  # 设置中文键盘
-    "resetKeyboard": True,  # 重置自动化时设置的键盘
-}
-driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+class App_config:
+    def setup(self):
+        desired_caps = {
+            "deviceName": "NDPBB23129202538",  # 启动的设备
+            "automationName": "Appium",  # 使用的自动化引擎，如appium（默认）或Selendroid
+            "platformName": "Android",  # 使用的移动平台，如Android或IOs
+            # adb shell getprop ro.build.version.release，获取设备Android系统版本（即platformVersion）
+            "platformVersion": "10",  # 指定的平台的系统版本，这里为安卓平台，12
+            # dumpsys window | grep mCurrentFocus
+            "appPackage": "com.orcbit.oladanceearphone",  # 被测试App的Package名
+            "appActivity": ".ui.activity.MainActivity",  # 被测试App的Activity名
+            "unicodeKeyboard": True,  # 设置中文键盘
+            "resetKeyboard": True,  # 重置自动化时设置的键盘
+        }
 
-# help(webdriver.Remote)
-
+        # desired_caps={}
+        # desired_caps['deviceName']:'NDPBB23129202538'
+        # desired_caps['automationName']: 'Appium'
+        # desired_caps['platformName']: 'Android'
+        # desired_caps['platformVersion']: '10'
+        # desired_caps['appPackage']: 'com.orcbit.oladanceearphone'
+        # desired_caps['appActivity']: ".ui.activity.MainActivity"
+        # desired_caps['unicodeKeyboard']: 'True'
+        # desired_caps['resetKeyboard']: 'True'
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    def teardown(self):
+     # 回收session
+       self.driver.quit()
+    def test_api_demo(self):
 # 1、使用ID定位（切换tab页）
-driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/iv_account").click()
-time.sleep(5)
+       self.driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/iv_account").click()
+
 # 2、使用ID定位（勾选同意用户协议）
-driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/iv_agree").click()
-time.sleep(5)
+       self.driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/iv_agree").click()
 # 3、使用ID定位（切换到手机号码登录）
-driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/tv_login_phone").click()
-time.sleep(5)
+       self.driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/tv_login_phone").click()
+
+       time.sleep(5)
+
 # 4、使用ID定位（先清除输入栏信息）
-driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/et_name").clear()
-time.sleep(5)
+       self.driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/et_name").clear()
+
 # 5、使用ID定位（输入手机号码登录）
-driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/et_name").send_keys("13570368266")
-time.sleep(5)
+       self.driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/et_name").send_keys("13570368266")
+
+       # time.sleep(5)
+
 # 6、使用ID定位（获取验证码）
-driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/tv_next").click()
-time.sleep(5)
+       self.driver.find_element(AppiumBy.ID,"com.orcbit.oladanceearphone:id/tv_next").click()
+
+       self.driver.implicitly_wait(5)
+
+
+
+
+
+print("执行完毕")
--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+from case.data import App_config
 
+# 要调用一个类里的函数，我们首先需要创建类的实例,实例就像是类的具体化身，它可以执行类里定义的函数。
+jk = App_config()
 
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+jk.setup()
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+jk.test_api_demo()
+
+# jk.teardown()
